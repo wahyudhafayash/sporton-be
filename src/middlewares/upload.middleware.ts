2f5344dc92244c4d724c4afd62cc42f93a61afc0
fayass <wahyudhafayash@gmail.com>
2026-02-05 07:37:22 +0700

    fix(upload) fixing typo upload middleware

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -23,7 +23,7 @@ const fileFilter = (
   file: Express.Multer.File,
   cb: multer.FileFilterCallback,
 ) => {
-  if (file.mimetype.startsWith("image'")) {
+  if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
     cb(new Error("Only images are allowed"));
